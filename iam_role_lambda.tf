b4300493afb568d054efc724997bdfd337a22513
Anorlondo448 <kanaend0605@gmail.com>
2018-09-07 18:26:09 +0900

    add iam role for lambda

---
+++ iam_role_lambda.tf
@@ -0,0 +1,23 @@
+###
+#
+# IAM Role for Lambda
+#
+resource "aws_iam_role" "lambda" {
+    name = "lambda"
+
+    assume_role_policy = <<POLICY
+{
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": "sts:AssumeRole",
+                "Principal": {
+                    "Service": "lambda.amazonaws.com"
+                },
+                "Effect": "Allow",
+                "Sid": ""
+            }
+        ]
+}
+POLICY
+}
